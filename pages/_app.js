8a3aee42bf85e9fb2f2bb31cbd7f96c03d989d64
Macbook <macbook@192.168.1.2>
2026-02-10 23:16:46 +0530

    new favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+// pages/_app.js
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico?v=2" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
